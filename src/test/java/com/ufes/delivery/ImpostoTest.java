281a5429450831944b3d14be708968a222acad99
Lucas Marques <lucas.fonseca@itixti.com.br>
2022-06-28 20:44:00 -0300

    commit inicial

---
+++ src/test/java/com/ufes/delivery/ImpostoTest.java
@@ -0,0 +1,24 @@
+package com.ufes.delivery;
+
+import com.ufes.delivery.model.Cliente;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ImpostoTest {
+
+    private Cliente cliente;
+
+    @BeforeEach
+    void beforeEach(){
+        this.cliente = new Cliente("Lucas Marques", 2022);
+    }
+
+    @Test
+    void CT011(){
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+}
